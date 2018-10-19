7792f8e1819b37ec7845d4bf79960c6c8fcf3253
M Nasimul Haque <nasim.haque@gmail.com>
2018-10-20 00:47:28 +0100

    show the overview metrics

--- ngapp/src/app/app.component.ts
+++ ngapp/src/app/app.component.ts
@@ -14,7 +14,33 @@ export class AppComponent {
         private metricsService: MetricsService
     ) {
         this.metricsService.getMetrics().subscribe(
-            data => this.files = data
+            data => this.setData(data)
         );
+    }
+
+    setData(data) {
+        this.files = data.map(x => {
+            let cc = 0, rank = '';
+            try {
+                x.cc.forEach(y => {
+                    if (y.complexity > cc) {
+                        cc = y.complexity;
+                        rank = y.rank;
+                    }
+                });
+            } catch (e) {
+            }
+
+            return {
+                ...x,
+                cc_value: cc,
+                cc_rank: rank
+            };
+        }).sort((a, b) => {
+            if (a.cc_value === b.cc_value) {
+                return a.mi - b.mi;
+            }
+            return b.cc_value - a.cc_value;
+        });
     }
 }
