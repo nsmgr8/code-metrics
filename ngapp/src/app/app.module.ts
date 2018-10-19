ea0317aad66373972f0faffb64a27589f8921fcd
M Nasimul Haque <nasim.haque@gmail.com>
2018-10-19 23:23:40 +0100

    add cdk virtual scrolling

--- ngapp/src/app/app.module.ts
+++ ngapp/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FileItemComponent } from './file-item/file-item.component';
@@ -14,6 +16,7 @@ import { FileItemComponent } from './file-item/file-item.component';
     imports: [
         BrowserModule,
         HttpClientModule,
+        ScrollingModule,
         AppRoutingModule
     ],
     providers: [],
